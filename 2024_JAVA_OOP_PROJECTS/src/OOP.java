62b5e318d5b2bb493239661e36a11e61efa4dfc2
'Maze711' <'badlonmazeclarion@gmail.com'>
2024-01-28 12:19:42 +0800

    Created Basic OOP & User Java files

---
+++ 2024_JAVA_OOP_PROJECTS/src/OOP.java
@@ -0,0 +1,10 @@
+public class OOP {
+	public static void main(String[] args) {
+		User u = new User();
+		u.name = "Maze";
+		u.membership = "Premium";
+		u.age = 18;
+		
+		System.out.println(u.membership);
+	}
+}
