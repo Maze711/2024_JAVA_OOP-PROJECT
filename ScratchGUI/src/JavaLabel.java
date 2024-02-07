c1ac5ef3cf1a78a21b45802d6678f7284ab89db6
'Maze711' <'badlonmazeclarion@gmail.com'>
2024-02-07 21:51:37 +0800

    Add some stuff about label

--- ScratchGUI/src/JavaLabel.java
+++ ScratchGUI/src/JavaLabel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -7,8 +9,15 @@ public class JavaLabel {
 		
 		// JLabel = a GUI display area for a string of text, an image or both
 		
+//		ImageIcon image = new ImageIcon(""); Getting image file from SRC
+		
 		JLabel label = new JLabel(); //Creating a global name for label
 		label.setText("Bro, do you even code?"); //Set text of label
+//		label.setIcon(image); // Displaying image if there's image
+//		label.setHorizontalTextPosition(JLabel.CENTER) // Set text LEFT, CENTER, RIGHT of image icon
+//		label.setVerticalTextPosition(JLabel.TOP); // Set text TOP, CENTER, BOTTOM of image icon
+		label.setForeground(new Color(0x00FF00)); // USE HEX CODE set font color of text
+		label.setFont(new Font("MV Boli",Font.PLAIN,20)); // set font of text
 		
 		JFrame frame = new JFrame(); //Creates a frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
