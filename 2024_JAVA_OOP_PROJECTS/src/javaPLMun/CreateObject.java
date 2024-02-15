4471665982e0a25b863ff0360a975783cbc97e06
'Maze711' <'badlonmazeclarion@gmail.com'>
2024-02-15 23:41:31 +0800

    OOP I guess, change later

---
+++ 2024_JAVA_OOP_PROJECTS/src/javaPLMun/CreateObject.java
@@ -0,0 +1,16 @@
+package javaPLMun;
+
+public class CreateObject {
+
+	static void BSCS2C() {
+		System.out.println("SHEESH");
+	}
+	
+	public static void main(String[] args) {
+
+		BSCS2C();
+
+		
+	}
+
+}
