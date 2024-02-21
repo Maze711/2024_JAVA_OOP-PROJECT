74693c39a2402aee7b717471359df3e5f3a00a25
'Maze711' <'badlonmazeclarion@gmail.com'>
2024-02-21 15:52:03 +0800

    Created a TestFraneDBCONN for separate DB Connection

---
+++ GUI TESTER/src/TestFrameDBCONN.java
@@ -0,0 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestFrameDBCONN {
+    // Declaring SQL classes
+    private Connection conn = null;
+
+    // DB Credentials
+    private final String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+    private final String DBname = "samplegui";
+    private final String DBurl = "jdbc:mysql://localhost:3306/" + DBname;
+    private final String dbUsername = "root";
+    private final String dbPassword = "";
+
+    public static void main(String[] args) {
+        new TestFrameDBCONN();
+    }
+
+    TestFrameDBCONN() {
+        Connect();
+    }
+
+    void Connect() {
+        try {
+            Class.forName(jdbcDriver);
+            conn = DriverManager.getConnection(DBurl, dbUsername, dbPassword);
+            if (conn != null) {
+                System.out.println("Successfully Connected to Database!");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } finally {
+            // Close the connection
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
